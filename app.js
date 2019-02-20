38edf128fc177dcbe6356a138fc7e01ff25806d2
dipesh <dipeshshakya106@gmail.com>
2019-02-20 09:15:53 +0545

    adding event listener

--- app.js
+++ app.js
@@ -8,3 +8,32 @@ GAME RULES:
 - The first player to reach 100 points on GLOBAL score wins the game
 
 */
+var scores,roundScore, acitvePlayer;
+
+scores=[0,0];
+roundScore = 0;
+acitvePlayer = 0;
+
+
+// document.querySelector('#current-'+acitvePlayer).textContent= dice;
+// var x = document.querySelector('#score-0').textContent;
+document.querySelector('.dice').style.display ="none";
+document.getElementById('score-0').textContent='0';
+document.getElementById('score-1').textContent='0';
+document.getElementById('current-0').textContent='0';
+document.getElementById('current-1').textContent='0';
+document.querySelector('.btn-roll').addEventListener('click',function(){
+
+	// 1.random number
+	var dice = Math.floor(Math.random()*6)+1;
+
+	// 2. Display the result
+	var diceDOM = document.querySelector('.dice'); 
+	diceDOM.style.display='block';
+	diceDOM.src = "dice-" +dice+'.png';
+
+
+
+	// 3.update the round score if the rolled number was not a 1
+
+});
